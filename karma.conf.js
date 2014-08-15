a028b6d30ed1a072ba56e8356049f26a973a469e
Sho Kusano <rosylilly@aduca.org>
2014-08-15 14:29:07 +0900

    Fix karma settings

--- karma.conf.js
+++ karma.conf.js
@@ -15,6 +15,7 @@ module.exports = function(config) {
 
     // list of files / patterns to load in the browser
     files: [
+      "test/**/*.js"
     ],
 
     browserify: {
@@ -56,7 +57,7 @@ module.exports = function(config) {
 
 
     // enable / disable watching file and executing tests whenever any file changes
-    autoWatch: false,
+    autoWatch: true,
 
 
     // start these browsers
