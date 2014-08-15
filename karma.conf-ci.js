c6b3c937997d65ce558a97603369d2fd297a1e8e
Sho Kusano <rosylilly@aduca.org>
2014-08-15 20:57:16 +0900

    Setup browser stack testing

--- karma.conf-ci.js
+++ karma.conf-ci.js
@@ -3,67 +3,131 @@
 var fs = require('fs');
 
 module.exports = function(config) {
-  // Use ENV vars on Travis and sauce.json locally to get credentials
-  if (!process.env.SAUCE_USERNAME) {
-    if (!fs.existsSync('sauce.json')) {
-      console.log('Create a sauce.json with your credentials based on the sauce-sample.json file.');
+  if (!process.env.BROWSER_STACK_USERNAME) {
+    if (!fs.existsSync('bs.json')) {
+      console.log('Create a bs.json with your credentials');
       process.exit(1);
     } else {
-      process.env.SAUCE_USERNAME = require('./sauce').username;
-      process.env.SAUCE_ACCESS_KEY = require('./sauce').accessKey;
+      process.env.BROWSER_STACK_USERNAME = require('./bs').username;
+      process.env.BROWSER_STACK_ACCESS_KEY = require('./bs').accessKey;
     }
   }
 
-  // Browsers to run on Sauce Labs
   var customLaunchers = {
-    'SL_Chrome': {
-      base: 'SauceLabs',
-      browserName: 'chrome',
-      platform: 'OS X 10.9',
-      version: '35'
+    // Internet Explorers
+    bs_ie7_win: {
+      base: 'BrowserStack',
+      browser: 'ie',
+      browser_version: '7.0',
+      os: 'Windows',
+      os_version: 'XP'
     },
-    'SL_Firefox': {
-      base: 'SauceLabs',
-      browserName: 'firefox',
-      platform: 'OS X 10.9',
-      version: '30'
+    bs_ie8_win: {
+      base: 'BrowserStack',
+      browser: 'ie',
+      browser_version: '8.0',
+      os: 'Windows',
+      os_version: '7'
     },
-    'SL_Safari': {
-      base: 'SauceLabs',
-      browserName: 'safari',
-      platform: 'OS X 10.9',
-      version: '7'
+    bs_ie9_win: {
+      base: 'BrowserStack',
+      browser: 'ie',
+      browser_version: '9.0',
+      os: 'Windows',
+      os_version: '7'
     },
-    'SL_IE_7': {
-      base: 'SauceLabs',
-      browserName: 'internet explorer',
-      platform: 'Windows XP',
-      version: '7'
+    bs_ie10_win: {
+      base: 'BrowserStack',
+      browser: 'ie',
+      browser_version: '10.0',
+      os: 'Windows',
+      os_version: '8'
     },
-    'SL_IE_8': {
-      base: 'SauceLabs',
-      browserName: 'internet explorer',
-      platform: 'Windows 7',
-      version: '8'
+    bs_ie11_win: {
+      base: 'BrowserStack',
+      browser: 'ie',
+      browser_version: '11.0',
+      os: 'Windows',
+      os_version: '8.1'
     },
-    'SL_IE_9': {
-      base: 'SauceLabs',
-      browserName: 'internet explorer',
-      platform: 'Windows 7',
-      version: '9'
+    // Chrome
+    bs_chrome_mac: {
+      base: 'BrowserStack',
+      browser: 'chrome',
+      browser_version: '36.0',
+      os: 'OS X',
+      os_version: 'Mavericks'
     },
-    'SL_IE_10': {
-      base: 'SauceLabs',
-      browserName: 'internet explorer',
-      platform: 'Windows 7',
-      version: '10'
+    // firefox
+    bs_firefox_mac: {
+      base: 'BrowserStack',
+      browser: 'firefox',
+      browser_version: '31.0',
+      os: 'OS X',
+      os_version: 'Mavericks'
     },
-    'SL_IE_11': {
-      base: 'SauceLabs',
-      browserName: 'internet explorer',
-      platform: 'Windows 8.1',
-      version: '11'
-    }
+    // Opera
+    bs_opera_mac: {
+      base: 'BrowserStack',
+      browser: 'opera',
+      browser_version: '12.15',
+      os: 'OS X',
+      os_version: 'Mavericks'
+    },
+    // Safari
+    bs_safari7_mac: {
+      base: 'BrowserStack',
+      browser: 'safari',
+      browser_version: '7',
+      os: 'OS X',
+      os_version: 'Mavericks'
+    },
+    bs_safari61_mac: {
+      base: 'BrowserStack',
+      browser: 'safari',
+      browser_version: '6.1',
+      os: 'OS X',
+      os_version: 'Mountain Lion'
+    },
+    bs_safari6_mac: {
+      base: 'BrowserStack',
+      browser: 'safari',
+      browser_version: '6',
+      os: 'OS X',
+      os_version: 'Lion'
+    },
+    // iPhone
+    bs_iphone5: {
+      base: 'BrowserStack',
+      device: 'iPhone 5',
+      os: 'ios',
+      os_version: '6.0'
+    },
+    bs_iphone5s: {
+      base: 'BrowserStack',
+      device: 'iPhone 5S',
+      os: 'ios',
+      os_version: '7.0'
+    },
+    // Android
+    bs_android_40: {
+      base: 'BrowserStack',
+      os: 'android',
+      os_version: '4.0',
+      device: 'Samsung Galaxy Nexus'
+    },
+    bs_android_41: {
+      base: 'BrowserStack',
+      os: 'android',
+      os_version: '4.1',
+      device: 'Samsung Galaxy S III'
+    },
+    bs_android_42: {
+      base: 'BrowserStack',
+      os: 'android',
+      os_version: '4.2',
+      device: 'LG Nexus 4'
+    },
   };
 
   config.set({
@@ -104,7 +168,7 @@ module.exports = function(config) {
     // test results reporter to use
     // possible values: 'dots', 'progress'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['dots', 'saucelabs'],
+    reporters: ['dots'],
 
 
     // web server port
@@ -119,14 +183,16 @@ module.exports = function(config) {
     // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
     logLevel: config.LOG_INFO,
 
-    sauceLabs: {
-      testName: 'magnet-inc/lookie'
+
+    browserStack: {
+      project: 'magnet-inc/lookie',
+      build: process.env.TRAVIS_BUILD_NUMBER || +(new Date)
     },
-    captureTimeout: 120000,
+
     customLaunchers: customLaunchers,
+    browsers: Object.keys(customLaunchers),
 
     autoWatch: false,
-    browsers: Object.keys(customLaunchers),
     singleRun: true
   });
 };
