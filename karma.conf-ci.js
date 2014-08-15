f347ed5d595b023697c47d70ff17245a69296f26
Sho Kusano <rosylilly@aduca.org>
2014-08-15 17:28:58 +0900

    Add env script

--- karma.conf-ci.js
+++ karma.conf-ci.js
@@ -2,6 +2,17 @@
 // Generated on Thu Aug 14 2014 04:56:24 GMT+0900 (JST)
 
 module.exports = function(config) {
+  // Use ENV vars on Travis and sauce.json locally to get credentials
+  if (!process.env.SAUCE_USERNAME) {
+    if (!fs.existsSync('sauce.json')) {
+      console.log('Create a sauce.json with your credentials based on the sauce-sample.json file.');
+      process.exit(1);
+    } else {
+      process.env.SAUCE_USERNAME = require('./sauce').username;
+      process.env.SAUCE_ACCESS_KEY = require('./sauce').accessKey;
+    }
+  }
+
   // Browsers to run on Sauce Labs
   var customLaunchers = {
     'SL_Chrome': {
