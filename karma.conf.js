3d4627ad79afc9687f3c74ce3dbb6b09ce850274
Sho Kusano <rosylilly@aduca.org>
2014-08-16 01:04:07 +0900

    Do not use expects.js

--- karma.conf.js
+++ karma.conf.js
@@ -10,7 +10,7 @@ module.exports = function(config) {
 
     // frameworks to use
     // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
-    frameworks: ['jasmine', 'expect', 'browserify'],
+    frameworks: ['jasmine', 'browserify'],
 
 
     // list of files / patterns to load in the browser
